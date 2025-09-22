5177ba789a6fba325794bf4dbdd0c4514754cd52
Yanamala1373 <yanamalamahesh130703@gmail.com>
2025-09-22 23:10:01 +0530

    NavigationController.java
    Third commit

--- PAMS_PRE_FINAL-master_PAMS_PRE_FINAL-master/src/main/java/project1/project1/control/NavigationController.java
+++ PAMS_PRE_FINAL-master_PAMS_PRE_FINAL-master/src/main/java/project1/project1/control/NavigationController.java
@@ -1,10 +1,12 @@
 package project1.project1.control;
 
-import jakarta.servlet.http.HttpSession;
-import jakarta.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class NavigationController {
@@ -17,6 +19,12 @@ public class NavigationController {
 
     // Book Now → new/existing user choice
     @GetMapping("/book-appointment")
+    public String bookNow() {
+        return "book_appointment"; // book_appointment.html
+    }
+    
+ // Book Appointmnet → new/existing user choice
+    @GetMapping("/bookAppointment")
     public String bookAppointment() {
         return "book_appointment"; // book_appointment.html
     }
@@ -29,9 +37,18 @@ public class NavigationController {
 
     // Show login form (Spring Security will handle POST /login)
     @GetMapping("/login")
-    public String showLoginForm() {
-        return "login"; // login.html
+    public String showLoginPage(@RequestParam(value = "error", required = false) String error,
+                                @RequestParam(value = "logout", required = false) String logout,
+                                Model model) {
+        if (error != null) {
+            model.addAttribute("error", "Invalid email or password or role Mismatch.\n Please try again.");
+        }
+        if (logout != null) {
+            model.addAttribute("message", "You have been logged out successfully.");
+        }
+        return "login"; // This should match your login.html
     }
+
 
     // Logout (works alongside Spring Security's logout)
     @GetMapping("/logout")
